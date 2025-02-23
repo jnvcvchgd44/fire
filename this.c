76c161138f3736accdae820e00c1ecce92d92ada
jnvcvchgd44 <jnvcvchgd44@gmail.com>
2025-02-23 15:26:06 +0800

    Modify this.c

--- this.c
+++ this.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# System present also question can mother production.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
